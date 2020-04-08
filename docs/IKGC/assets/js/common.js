042145761c1354634678c0c279bca0eef603280b
Ease112 <imagease@gmail.com>
2020-04-08 14:08:00 +0900

    international版草案

---
+++ docs/IKGC/assets/js/common.js
@@ -0,0 +1,4 @@
+/* --------------------------------------------------- */
+/* コピーライト書き換え                                           
+/* --------------------------------------------------- */
+$("small").text("Planning Board Members, Special Interest Group on Semantic Web and Ontology, the Japanese Society for AI");
